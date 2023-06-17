1ec598cfaac4fdd04082c80c64271fb8ed2dfc47
name <you@example.com>
2023-06-17 09:36:45 +0000

    update form component

--- components/form.tsx
+++ components/form.tsx
@@ -1,59 +1,202 @@
 "use client";
 
-import { ConnectWallet } from "@thirdweb-dev/react";
+import { ConnectWallet, useAddress } from "@thirdweb-dev/react";
 import { useState } from "react";
 import styled from "styled-components";
 
+const INITIAL_STATE = {
+  tokenName: "",
+  symbol: "",
+  initialSupply: "",
+  decimals: "",
+  mintable: false,
+  burnable: false,
+  pausable: false,
+  ownable: false,
+};
+
 export default function Form() {
-  const [count, setCount] = useState(0);
+  const address = useAddress();
+
+  const [tokenName, setTokenName] = useState("");
+  const [symbol, setSymbol] = useState("");
+  const [initialSupply, setInitialSupply] = useState("");
+  const [decimals, setDecimals] = useState("");
+  const [mintable, setMintable] = useState(false);
+  const [burnable, setBurnable] = useState(false);
+  const [pausable, setPausable] = useState(false);
+  const [ownable, setOwnable] = useState(false);
+  const [tokenNameError, setTokenNameError] = useState("");
+  const [symbolError, setSymbolError] = useState("");
+  const [initialSupplyError, setInitialSupplyError] = useState("");
+  const [decimalsError, setDecimalsError] = useState("");
+  const [addressError, setAddressError] = useState("");
+
+  const createToken = () => {
+    // Reset previous error messages
+    setTokenNameError("");
+    setSymbolError("");
+    setInitialSupplyError("");
+    setDecimalsError("");
+    setAddressError("");
+
+    let hasErrors = false;
+
+    if (tokenName === "") {
+      setTokenNameError("Token name is required.");
+      hasErrors = true;
+    }
+    if (symbol === "") {
+      setSymbolError("Symbol is required.");
+      hasErrors = true;
+    }
+    if (initialSupply === "") {
+      setInitialSupplyError("Initial supply is required.");
+      hasErrors = true;
+    }
+    if (decimals === "") {
+      setDecimalsError("Decimals is required.");
+      hasErrors = true;
+    }
+    if (!address) {
+      setAddressError("Please connect your wallet.");
+      hasErrors = true;
+    }
+
+    if (hasErrors) {
+      return;
+    }
+
+    console.log("Token name:", tokenName);
+    console.log("Symbol:", symbol);
+    console.log("Initial supply:", initialSupply);
+    console.log("Decimals:", decimals);
+    console.log("Mintable:", mintable);
+    console.log("Burnable:", burnable);
+    console.log("Pausable:", pausable);
+    console.log("Ownable:", ownable);
+
+    setTokenName(INITIAL_STATE.tokenName);
+    setSymbol(INITIAL_STATE.symbol);
+    setInitialSupply(INITIAL_STATE.initialSupply);
+    setDecimals(INITIAL_STATE.decimals);
+    setMintable(INITIAL_STATE.mintable);
+    setBurnable(INITIAL_STATE.burnable);
+    setPausable(INITIAL_STATE.pausable);
+    setOwnable(INITIAL_STATE.ownable);
+  };
+
+  const handleTokenNameChange = (e) => {
+    setTokenName(e.target.value);
+    setTokenNameError("");
+  };
+
+  const handleSymbolChange = (e) => {
+    setSymbol(e.target.value);
+    setSymbolError("");
+  };
+
+  const handleInitialSupplyChange = (e) => {
+    setInitialSupply(e.target.value);
+    setInitialSupplyError("");
+  };
+
+  const handleDecimalsChange = (e) => {
+    setDecimals(e.target.value);
+    setDecimalsError("");
+  };
+
   return (
     <>
       <Row>
         <Column>
           <InputContainer>
             <Label>Token name</Label>
-            <Input placeholder="e.g. My Token Name" />
+            <Input
+              placeholder="e.g. My Token Name"
+              value={tokenName}
+              onChange={handleTokenNameChange}
+            />
+            {tokenNameError && <ErrorMessage>{tokenNameError}</ErrorMessage>}
           </InputContainer>
           <InputContainer>
             <Label>Symbol</Label>
-            <Input placeholder="e.g. SYM" />
+            <Input
+              placeholder="e.g. SYM"
+              value={symbol}
+              onChange={handleSymbolChange}
+            />
+            {symbolError && <ErrorMessage>{symbolError}</ErrorMessage>}
           </InputContainer>
           <InputContainer>
             <Label>Initial supply</Label>
-            <Input type="number" min="0" placeholder="e.g. 123456789" />
+            <Input
+              type="number"
+              min="0"
+              placeholder="e.g. 123456789"
+              value={initialSupply}
+              onChange={handleInitialSupplyChange}
+            />
+            {initialSupplyError && (
+              <ErrorMessage>{initialSupplyError}</ErrorMessage>
+            )}
           </InputContainer>
           <InputContainer>
             <Label>Decimals (0-18)</Label>
-            <Input type="number" min="0" placeholder="e.g. 18" />
-          </InputContainer>
-          <InputContainer>
-            <Label>Decimals (0-18)</Label>
-            <Input type="number" min="0" placeholder="e.g. 18" />
-          </InputContainer>
-          <InputContainer>
-            <Label>
-              <Checkbox id="mintable"/> Mintable
-             </Label>
-          </InputContainer>
-          <InputContainer>
-            <Label>
-              <Checkbox id="burnable"/> Burnable
-             </Label>
+            <Input
+              type="number"
+              min="0"
+              placeholder="e.g. 18"
+              value={decimals}
+              onChange={handleDecimalsChange}
+            />
+            {decimalsError && <ErrorMessage>{decimalsError}</ErrorMessage>}
           </InputContainer>
           <InputContainer>
             <Label>
-              <Checkbox id="pausable"/> Pausable
-             </Label>
+              <Checkbox
+                id="mintable"
+                checked={mintable}
+                onChange={() => setMintable(!mintable)}
+              />
+              Mintable
+            </Label>
           </InputContainer>
           <InputContainer>
             <Label>
-              <Checkbox id="ownable"/> Ownable
-             </Label>
+              <Checkbox
+                id="burnable"
+                checked={burnable}
+                onChange={() => setBurnable(!burnable)}
+              />
+              Burnable
+            </Label>
+          </InputContainer>
+          <InputContainer>
+            <Label>
+              <Checkbox
+                id="pausable"
+                checked={pausable}
+                onChange={() => setPausable(!pausable)}
+              />
+              Pausable
+            </Label>
+          </InputContainer>
+          <InputContainer>
+            <Label>
+              <Checkbox
+                id="ownable"
+                checked={ownable}
+                onChange={() => setOwnable(!ownable)}
+              />
+              Ownable
+            </Label>
           </InputContainer>
         </Column>
         <Column></Column>
       </Row>
-      <Button>Create Token</Button>
+      <Button onClick={createToken}>Create Token</Button>
+      <div>{addressError && <ErrorMessage>{addressError}</ErrorMessage>}</div>
     </>
   );
 }
@@ -103,6 +246,13 @@ const Button = styled.button`
 const Checkbox = styled.input.attrs({ type: "checkbox" })`
   font-size: 1.5em;
   margin: 0;
+  margin-right: 5px;
   hieght: 20px;
   width: 20px;
+`;
+
+const ErrorMessage = styled.span`
+  color: red;
+  font-size: 12px;
+  margin-top: 5px;
 `;
